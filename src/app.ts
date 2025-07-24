b81abcd6c1f5012bca42348746eac0f20eeb536f
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-07-24 15:51:37 +0700

    video 30

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import 'dotenv/config';
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.send("Hello World!!!!")
