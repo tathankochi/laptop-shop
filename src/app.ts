afe0116465f773dfeb4d776546ed900b42f0d2eb
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-07-24 16:24:17 +0700

    video 32

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,12 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+//config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.get("/", (req, res) => {
-    res.send("Hello World!!!!")
+    res.render("home.ejs")
 })
 
 app.listen(PORT, () => {
