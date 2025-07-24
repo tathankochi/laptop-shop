70e611d77a7b440a3e14be527fb77dd46decb2d9
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-07-24 14:49:19 +0700

    video 24

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(PORT, () => {
+    console.log(`My app is running on port: ${PORT}`)
+});
