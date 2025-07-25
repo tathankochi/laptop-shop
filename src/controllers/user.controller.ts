bdd5fd52eafbe691f711eb7c30fb6ad3a92a8102
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-07-26 01:47:09 +0700

    video 39

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { handleCreateUser } from "../services/user.service";
 const getHomePage = (req: Request, res: Response) => {
     return res.render("home.ejs");
 }
@@ -7,6 +8,8 @@ const getCreateUserPage = (req: Request, res: Response) => {
     return res.render("create-user");
 }
 const postCreateUser = (req: Request, res: Response) => {
+    const { fullName, email, address } = req.body;
+    handleCreateUser(fullName, email, address);
     return res.redirect("/");
 }
 
---
+++ src/services/user.service.ts
@@ -0,0 +1,4 @@
+const handleCreateUser = (fullName: string, email: string, address: string) => {
+    console.log("Insert a new user");
+}
+export { handleCreateUser }
