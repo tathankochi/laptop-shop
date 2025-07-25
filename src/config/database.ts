123d479004e9216e2015eaf5be5fdfbdbeef055c
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-07-26 03:08:26 +0700

    video 45

---
+++ src/config/database.ts
@@ -0,0 +1,27 @@
+// Get the client
+import mysql from 'mysql2/promise';
+
+
+const getConnection = async () => {
+    // Create the connection to database
+    const connection = await mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'nodejspro',
+    })
+    try {
+        const [results, fields] = await connection.query(
+            'SELECT * FROM `users`'
+        );
+
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+export default getConnection;
