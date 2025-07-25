907dc31621df6c0777b5f49fd93400efba353a8c
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-07-26 00:03:15 +0700

    video 34

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import 'dotenv/config';
+import webRoutes from "./routes/web";
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -7,9 +9,8 @@ const PORT = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.get("/", (req, res) => {
-    res.render("home.ejs")
-})
+//config routes
+webRoutes(app);
 
 app.listen(PORT, () => {
     console.log(`My app is running on port: ${PORT}`)
