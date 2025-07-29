71456d2c3b8de1d7e40ba169b2efc1b8920b7960
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-07-29 09:54:23 +0700

    video 84

--- src/config/seed.ts
+++ src/config/seed.ts
@@ -1,19 +1,24 @@
 import { prisma } from "config/client"
+import { hashPassword } from "services/user.service";
+import { ACCOUNT_TYPE } from "./constant";
 const initDataBase = async () => {
     const countUser = await prisma.user.count();
     const countRole = await prisma.role.count();
     if (countUser === 0) {
+        const defaultPassword = await hashPassword("123456");
         await prisma.user.createMany({
             data: [
                 {
+                    fullName: "tathan",
                     username: "tathankochi@gmail.com",
-                    password: "123456",
-                    accountType: "SYSTEM"
+                    password: defaultPassword,
+                    accountType: ACCOUNT_TYPE.SYSTEM
                 },
                 {
+                    fullName: "Admin",
                     username: "haobla@gmail.com",
-                    password: "123456",
-                    accountType: "SYSTEM"
+                    password: defaultPassword,
+                    accountType: ACCOUNT_TYPE.SYSTEM
                 }
             ]
         })
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,15 +1,21 @@
 import getConnection from "config/database";
 import { prisma } from "config/client";
 import { ACCOUNT_TYPE } from "config/constant";
+import bcrypt from "bcrypt";
+const saltRounds = 10;
 
+const hashPassword = async (plainText: string) => {
+    return await bcrypt.hash(plainText, saltRounds);
+}
 const handleCreateUser = async (fullName: string, email: string, address: string, phone: string, avatar: string) => {
     //Insert a new user
     try {
+        const defaultPassword = await hashPassword("123456");
         const newUser = await prisma.user.create({
             data: {
                 fullName: fullName,
                 username: email,
-                password: "123456",
+                password: defaultPassword,
                 address: address,
                 accountType: ACCOUNT_TYPE.SYSTEM,
                 avatar: avatar,
@@ -67,4 +73,4 @@ const updateUserById = async (id: string, name: string, email: string, address: 
 }
 
 
-export { handleCreateUser, handleDeleteUser, getAllUsers, getUserById, updateUserById, getAllRoles }
+export { handleCreateUser, handleDeleteUser, getAllUsers, getUserById, updateUserById, getAllRoles, hashPassword }
