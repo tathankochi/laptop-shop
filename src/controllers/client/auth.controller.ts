9b5c9cd460aa4716433f763f7a1c632158f456a9
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-08-23 16:00:26 +0700

    video 103

---
+++ src/controllers/client/auth.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+const getLoginPage = (req: Request, res: Response) => {
+    return res.render("client/auth/login.ejs");
+}
+
+const getRegisterPage = (req: Request, res: Response) => {
+    return res.render("client/auth/register.ejs");
+}
+export { getLoginPage, getRegisterPage };
